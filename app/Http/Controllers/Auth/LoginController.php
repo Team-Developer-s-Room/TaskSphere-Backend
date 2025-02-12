0e50becc707922cd5bf7466de82367a6556491f6
COLLINS <collincity111@gmail.com>
2025-02-12 08:32:13 +0100

    Updated the login functionality

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\Auth\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -16,15 +17,29 @@ class LoginController extends Controller
             "password" > "required"
         ]);
 
-        if (auth()->attempt($validated)) {
-            $user = auth()->user();
-            $token = $user->createToken('API Token')->plainTextToken;
-
-            return response()->json([
-                'data' => new UserResource($user),
-                'token' => $token,
-                'message' => 'User registered successfully',
-            ], Response::HTTP_OK);
+        if (!Auth::attempt($validated)) {
+            abort(response()->json([
+                'message' => 'Invalid credentials',
+            ], Response::HTTP_UNAUTHORIZED));
         }
+
+        $user = Auth::user();
+        $token = $user->createToken('API Token')->plainTextToken;
+
+        return response()->json([
+            'data' => new UserResource($user),
+            'token' => $token,
+            'message' => 'User logged in successfully',
+        ], Response::HTTP_OK);
+    }
+
+    public function logout(Request $request)
+    {
+        // $request->user()->tokens()->delete();
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'User logged out successfully',
+        ], Response::HTTP_OK);
     }
 }
